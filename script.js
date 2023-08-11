48c478f1ae3f6dd72e9c83163b2176e830320e8c
uday634 <uk8096386837@gmail.com>
2023-08-11 22:16:45 +0530

    2 commit

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-//local storage
-localStorage.setItem('name', 'bob')
-console.log(localStorage.getItem('name'))
-localStorage.removeItem('name')
+// //local storage
+// localStorage.setItem('name', 'bob')
+// console.log(localStorage.getItem('name'))
+// localStorage.removeItem('name')
 
-//session Storage
-sessionStorage.setItem('name', 'uday')
-console.log(sessionStorage.getItem('name'))
-sessionStorage.removeItem('name')
+// //session Storage
+// sessionStorage.setItem('name', 'uday')
+// console.log(sessionStorage.getItem('name'))
+// sessionStorage.removeItem('name')
 
-document.cookie ='name=Kyle; expires=' + new Date(9999,3,1).toUTCString()
+// document.cookie ='name=Kyle; expires=' + new Date(9999,3,1).toUTCString()
+
+let myObj = {
+    name: 'Domeic',
+    age:49
+}
+
+localStorage.setItem('myObj',myObj)
+let myObj_serialzed = JSON.stringify(myObj);
+localStorage.setItem('myObj',myObj_serialzed);
+let myObj_derialzed = JSON.parse(localStorage.getItem('myObj'));
+console.log(myObj_derialzed)
